a0878dc2b891cea4b9c59c257fb3a0ffe993b804
Marivaldo Pereira dos Santos <marivaldo.santos42@gmail.com>
2022-04-20 18:04:44 -0300

    estado e cidade

---
+++ src/main/java/com/javamar/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.javamar.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javamar.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
