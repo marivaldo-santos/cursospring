43625ef9ac247297da2876ddc157448dfea975b8
Marivaldo Pereira dos Santos <marivaldo.santos42@gmail.com>
2022-04-20 13:10:04 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/javamar/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.javamar.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javamar.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+}
