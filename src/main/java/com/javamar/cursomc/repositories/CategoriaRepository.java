e49e9039e9d8f6a4c4aef4013e3567ae1517e8ef
Marivaldo Pereira dos Santos <marivaldo.santos42@gmail.com>
2022-04-20 11:31:44 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/javamar/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.javamar.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javamar.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+}
